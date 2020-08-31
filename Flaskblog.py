236bd2329a7b93a9ae1f337edd81e00a9153d51a
kevinsandry <kevinsandry@users.noreply.github.com>
2020-08-31 13:52:45 +0700

    add posts

--- Flaskblog.py
+++ Flaskblog.py
@@ -1,14 +1,36 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author':'Kevin Sandryan',
+        'title': 'Blog Post 1',
+        'content': 'first post content',
+        'date_posted': 'September 1, 2020'
+    },
+     {
+        'author':'John Doe',
+        'title': 'Blog Post 2',
+        'content': 'second post content',
+        'date_posted': 'September 2, 2020'
+    },
+     {
+        'author':'Stephen Jordan',
+        'title': 'Blog Post 3',
+        'content': 'third post content',
+        'date_posted': 'September 3, 2020'
+    }
+
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3003)
