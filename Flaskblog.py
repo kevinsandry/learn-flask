474f39158ae8c3512abae286461350808cafb84b
kevinsandry <kevinsandry@users.noreply.github.com>
2020-08-31 23:57:12 +0700

    add register and login

--- Flaskblog.py
+++ Flaskblog.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '1d75f0b0cdb877293206b6b6edfeae0b'
 
 posts = [
     {
@@ -31,6 +35,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True, port=3003)
